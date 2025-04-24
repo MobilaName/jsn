e9c1cd9770a87de723147aaa2cbef3d5caac8b61
larchanka <larchanka@me.com>
2025-04-24 22:24:53 +0200

    Vars and secrets change

--- src/modals/VarsAndSecrets.tsx
+++ src/modals/VarsAndSecrets.tsx
@@ -1,17 +1,63 @@
-import { useState } from "react";
+import { useCallback, useReducer } from "react";
 import { VarsAndSecretsType } from "../types";
 import { AiFillPlusCircle, AiTwotoneLock, AiTwotoneUnlock } from "react-icons/ai";
 import { TbTrashFilled } from "react-icons/tb";
+
+type VarsAndSecretsActionType = {
+  type: 'add' | 'remove' | 'edit' | 'type',
+  payload?: VarsAndSecretsActionPayloadType
+}
+
+type VarsAndSecretsActionPayloadType = [number, ('name' | 'value')?, string?]
+
+function reducer(state:VarsAndSecretsType[], action: VarsAndSecretsActionType) {
+  switch (action.type) {
+    case 'add': {
+      const newState = [...state, { name: '', value: '', type: 'var' }];
+      return newState;
+    }
+    case 'edit': {
+      const newState = [...state];
+      const [ index, field, value ] = action.payload || [];
+      
+      if (typeof index !== 'number' || !field) return state;
+
+      newState[index][field] = value || '';
+      return newState;
+    }
+    case 'remove': {
+      const newState = [...state];
+      const [ index ] = action.payload || [];
+      if (typeof index === 'number')
+        newState.splice(index, 1);
+      return newState;
+    }
+    case 'type': {
+      const newState = [...state];
+      const [ index, , newType = 'var' ] = action.payload || [] as any;
+
+      if (typeof index !== 'number') return state;
+
+      newState[index].type = newType;
+      newState[index].value = newType === 'secret' ? newState[index].value : '';
+      return newState;
+    }
+    default: {
+      return state;
+    }
+  }
+}
 
 export const VarsAndSecrets = ({ isVisible, handleCancel }: {
   isVisible: boolean,
   handleCancel: () => void
 }) => {
   const storedVars:VarsAndSecretsType[] = JSON.parse(localStorage.getItem('varsAndSecrets') || '[]');
-  const formattedVars = storedVars.map((varAndSecret:VarsAndSecretsType) => (
+  const formattedVars = storedVars.map(((varAndSecret:VarsAndSecretsType) => (
     { ...varAndSecret, value: varAndSecret.type === 'secret' ? atob(varAndSecret.value) : varAndSecret.value }
-  ));
-  const [vars, setVars] = useState(formattedVars);
+  )));
+  // @ts-ignore
+  const [vars, dispatch] = useReducer(reducer, formattedVars);
 
   const handleModalSave = (event: React.FormEvent) => {
     event.preventDefault();
@@ -22,30 +68,23 @@ export const VarsAndSecrets = ({ isVisible, handleCancel }: {
   };
 
   const handleChange = (field: 'name' | 'value', index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    const updatedVars = [...vars];
-    updatedVars[index][field] = event.target.value;
-    setVars(updatedVars);
+    dispatch({ type: 'edit', payload: [index, field, event.target.value] });
   };
 
   const handleAddVar = () => {
-    const updatedVars:VarsAndSecretsType[] = [...vars, { name: '', value: '', type: 'secret' }];
-    setVars(updatedVars);
+    dispatch({ type: 'add' });
   };
 
   const handleTypeChange = (index: number) => () => {
-    const updatedVars = [...vars];
-    updatedVars[index].value = updatedVars[index].type === 'secret' ? '' : updatedVars[index].value;
-    updatedVars[index].type = updatedVars[index].type === 'secret' ? 'var' : 'secret';
-    setVars([...updatedVars]);
+    dispatch({ type: 'type', payload: [index, undefined, vars[index].type === 'secret' ? 'var' : 'secret'] });
   };
 
   const deleteVar = (index: number) => () => {
     if (!confirm('Are you sure you want to delete this variable?')) return;
-    const updatedVars = [...vars];
-    updatedVars.splice(index, 1);
-    setVars([...updatedVars]);
+    
+    dispatch({ type: 'remove', payload: [index] });
   };
-
+  
   return (
     <dialog id="vars-and-secrets-modal" open={isVisible}>
       <form onSubmit={handleModalSave}>
@@ -59,11 +98,24 @@ export const VarsAndSecrets = ({ isVisible, handleCancel }: {
           <ul>
             {vars.map((varAndSecret, index) => (
               <li key={index + btoa(varAndSecret.value)}>
-                <input placeholder="Variable Name" id={`name-${index}`} type="text" defaultValue={varAndSecret.name} onChange={handleChange('name', index)} />
-                <input placeholder="Variable Value" id={`var-${index}`} type={varAndSecret.type === 'secret' ? 'password' : 'text'} defaultValue={varAndSecret.value} onChange={handleChange('value', index)} />
+                <input
+                  placeholder="Variable Name"
+                  id={`name-${index}`}
+                  type="text"
+                  defaultValue={varAndSecret.name}
+                  onChange={handleChange('name', index)}
+                />
+                <input
+                  placeholder="Variable Value"
+                  id={`var-${index}`}
+                  type={varAndSecret.type === 'secret' ? 'password' : 'text'}
+                  defaultValue={varAndSecret.value}
+                  onChange={handleChange('value', index)}
+                />
                 <button
                   className="secret-toggle"
-                  type="button" data-title={varAndSecret.type === 'secret' ? 'Make public' : 'Make secret'}
+                  type="button"
+                  data-title={varAndSecret.type === 'secret' ? 'Make public' : 'Make secret'}
                   onClick={handleTypeChange(index)}
                 >
                   {varAndSecret.type === 'secret' ? (
